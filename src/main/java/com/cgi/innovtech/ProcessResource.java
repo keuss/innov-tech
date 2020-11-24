ac3a80fc28a509cfc68364846f7a061c2f1abbeb
keuss <guillaume.gallois@gmail.com>
2020-11-24 18:52:35 +0100

    Add variables to process

--- src/main/java/com/cgi/innovtech/ProcessResource.java
+++ src/main/java/com/cgi/innovtech/ProcessResource.java
@@ -8,6 +8,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Map;
+
 @Slf4j
 @RequiredArgsConstructor
 @RestController
@@ -21,9 +25,11 @@ public class ProcessResource {
     public String startProcess(
             @ApiParam(value = "Nom du processus", required = true, example = "innov-tech-process") @PathVariable("processName") String processName) {
         log.info("Start process ...");
+        // http://localhost:8080/swagger-ui/index.html
         // see at http://localhost:8080/camunda/app/cockpit/
         // add http://localhost:8080/engine-rest/engine/default/process-definition
-        return runtimeService.startProcessInstanceByKey(processName).getProcessInstanceId();
+        Map<String, Object> variables = Collections.singletonMap("date", LocalDateTime.now());
+        return runtimeService.startProcessInstanceByKey(processName, variables).getProcessInstanceId();
     }
 
 }
